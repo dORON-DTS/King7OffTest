a569bb1f48f48e6aa1d6d03e763dca0a2475140c
dORON-DTS <doron.dts@gmail.com>
2025-07-20 03:54:10 +0300

    🔧 Fix: Add loading=async and enhance Google Autocomplete CSS

--- src/components/GooglePlacesAutocomplete.tsx
+++ src/components/GooglePlacesAutocomplete.tsx
@@ -49,7 +49,7 @@ const GooglePlacesAutocomplete: React.FC<GooglePlacesAutocompleteProps> = ({
 
       // Load the script
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&loading=async`;
       script.async = true;
       script.defer = true;
       script.onload = () => {
